c7475f2103758408c4f373968bff4d8597f8f23c
Valentin Honoré <valentin.honore@ensiie.fr>
2024-02-06 16:48:22 +0100

    fixed comm matrix, coming : plotting + eval scripts

--- apps/pallas_comm_matrix.cpp
+++ apps/pallas_comm_matrix.cpp
@@ -10,20 +10,7 @@
 #include "pallas/pallas_read.h"
 #include "pallas/pallas_storage.h"
 #include "pallas/pallas_write.h"
-#include <cinttypes>
-
-
-// #include "pallas/pallas.h"
-// #include "pallas/pallas_archive.h"
-// #include "pallas/pallas_hash.h"
-// #include "pallas/pallas_parameter_handler.h"
-// #include "pallas/pallas_storage.h"
-// #include "pallas/pallas_timestamp.h"
-// #include "pallas/pallas_write.h"
-
-static bool verbose = true;
-
-
+#include  <fstream>
 
 
 void pop_data(pallas::Event* e, void* data, size_t data_size, byte*& cursor) {
@@ -43,152 +30,61 @@ void pop_data(pallas::Event* e, void* data, size_t data_size, byte*& cursor) {
 
 
 
-/**
- * Compare the timestamps of the current token on each thread and select the smallest timestamp.
- * @returns Tuple containing the ThreadId and a TokenOccurence.
- *          You are responsible for the memory of the TokenOccurence.
- */
-static std::tuple<pallas::ThreadId, pallas::TokenOccurence> getNextToken(std::vector<pallas::ThreadReader>& threadReaders) {
-  // Find the earliest threadReader
-  pallas::ThreadReader* earliestReader = nullptr;
-  for (auto& reader : threadReaders) {
-    // Check if reader has finished reading its trace
-    if (reader.current_frame < 0)
-      continue;
-    if (!earliestReader || earliestReader->referential_timestamp > reader.referential_timestamp) {
-      earliestReader = &reader;
-    }
-  }
-
-  // If no reader was available
-  if (!earliestReader) {
-    return {PALLAS_THREAD_ID_INVALID, {nullptr, nullptr}};
-  }
-
-  // Grab the interesting information
-  auto& token = earliestReader->getCurToken();
-  auto* occurrence = earliestReader->getOccurence(token, earliestReader->tokenCount[token]);
-  auto threadId = earliestReader->thread_trace->id;
-
-  // Update the reader
-  earliestReader->updateReadCurToken();
-  if (token.type == pallas::TypeEvent)
-    earliestReader->moveToNextToken();
-
-  return {threadId, {&token, occurrence}};
-}
-
-
-
-
-
-/* Treat one event */
-static void read_event(const pallas::Thread* thread,
-                        const pallas::Token token,
-                        const pallas::EventOccurence* e,
-                       int**& matrix) {
-
-  byte* cursor = nullptr;
-  if (e->event->record == pallas::PALLAS_EVENT_MPI_SEND) {
-
-      /* Get attributes of the MPI_SEND calls */
-      uint32_t receiver;
-      uint32_t communicator;
-      uint32_t msgTag;
-      uint64_t msgLength;
-
-      pop_data(e->event, &receiver, sizeof(receiver), cursor);
-      pop_data(e->event, &communicator, sizeof(communicator), cursor);
-      pop_data(e->event, &msgTag, sizeof(msgTag), cursor);
-      pop_data(e->event, &msgLength, sizeof(msgLength), cursor);
-
-      /* Increment comm matrix for the Thread thread and the thread receiver receiver with the message size msgLength  */
-      matrix[thread->id][receiver]+=msgLength;
-
-      if (verbose) {
-        thread->printToken(token);
-        printf("MPI_SEND(dest=%d, comm=%x, tag=%x, len=%" PRIu64 "", receiver, communicator, msgTag, msgLength);
-        std::cout << "\t";
-     }
-  }
-
-  if (verbose)
-    std::cout << std::endl;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Treat one token */
-static void read_token(const pallas::Thread* thread,
-                        const pallas::Token* t,
-                        const pallas::Occurence* e,
-                        int**& matrix
-                      ) {
-   if (verbose) {
-    printf("Reading repeated_token(%d.%d) for thread %s\n", t->type, t->id, thread->getName());
-   }
-
-  // Prints the repeated_token we first started with
-  switch (t->type) {
-    case pallas::TypeInvalid:
-      pallas_error("Type is invalid\n");
-      break;
-    case pallas::TypeEvent:
-      read_event(thread, *t, &e->event_occurence,matrix);
-      break;
-    default:
-      break;
-  }
-}
-
-
-
-
 /** Explore all the events of all the threads sorted by timestamp*/
  void compute_matrix(pallas::Archive& trace, int**& matrix){
+   /* For each thread */
+   for (int i = 0; i < trace.nb_threads; i++) {
+     /* For each event in this thread */
+     pallas::Thread* t = trace.threads[i];
+     for (unsigned j = 0; j < t->nb_events; j++) {
 
-  auto readers = std::vector<pallas::ThreadReader>();
-  int reader_options = pallas::ThreadReaderOptions::None;
+       pallas::EventSummary e = t->events[j];
+       /* If the event is a MPI_send */
+       if (e.event.record == pallas::PALLAS_EVENT_MPI_SEND)
+       {
+          byte* cursor = nullptr;
 
-  for (int i = 0; i < trace.nb_threads; i++) {
-    readers.emplace_back(&trace, trace.threads[i]->id, reader_options);
+          /* Get attributes of the MPI_SEND calls */
+          uint32_t receiver;
+          uint32_t communicator;
+          uint32_t msgTag;
+          uint64_t msgLength;
+
+          pop_data(&e.event, &receiver, sizeof(receiver), cursor);
+          pop_data(&e.event, &communicator, sizeof(communicator), cursor);
+          pop_data(&e.event, &msgTag, sizeof(msgTag), cursor);
+          pop_data(&e.event, &msgLength, sizeof(msgLength), cursor);
+
+          /* Update comm matrix for the thread and receiver with the message size msgLength  */
+          matrix[i][receiver]+=msgLength;
+
+       }
+    }
   }
-  pallas::ThreadId threadId;
-  pallas::TokenOccurence tokenOccurence;
-  std::tie(threadId, tokenOccurence) = getNextToken(readers);
-  while (threadId != PALLAS_THREAD_ID_INVALID) {
-    auto currentReader = std::find_if(readers.begin(), readers.end(), [&threadId](const pallas::ThreadReader& reader) {
-      return reader.thread_trace->id == threadId;
-    });
-    read_token(currentReader->thread_trace, tokenOccurence.token, tokenOccurence.occurence,matrix);
-    // If you read the doc, you'll know that the memory of tokenOccurence is ours to manage
-    std::tie(threadId, tokenOccurence) = getNextToken(readers);
+ }
+
+
+
+
+void save_matrix(std::string& filename, int** matrix, int size){
+  std::ofstream outfile;
+  outfile.open(filename.c_str());
+
+  for (int i=0; i<size; i++)
+    outfile<<i+1<<",";
+  outfile<<std::endl;
+
+  for (int i = 0; i < size; i++) {
+      outfile<<i+1<<",";
+      for (int j = 0; j < size; j++) {
+          outfile << matrix[i][j];
+          if (j<size-1)
+            outfile << ",";
+      }
+      outfile<<std::endl;
   }
 }
 
-
-
-
-void save_matrix(std::string& filename, int** matrix){
-    std::cout<<"Currently writing the save matrix function"<<std::endl;
-
-    // remove warning temporarily
-    std::cout<<"!!! Coming soon : function to save "<<filename<<" trace name !!!"<<std::endl;
-    matrix[0][0]=0;
-    //TODO
-}
 
 
 void usage(const std::string& prog_name) {
@@ -254,7 +150,7 @@ int main(int argc, char** argv) {
 
 
   /* Save the matrix in the file */
-  save_matrix(output_file, matrix);
+  save_matrix(output_file, matrix,size);
 
   delete matrix;
 
