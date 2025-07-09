9db222d1e7450b61c84568f67461df6f9ab4696c
MONTICELLI Nikolai <n.monticelli2016@gmail.com>
2025-07-09 15:59:37 +0200

    ajout du calcul de l'overhead

--- analysis/prog/nas_overhead.sh
+++ analysis/prog/nas_overhead.sh
@@ -2,13 +2,29 @@
 
 nas_dir=$PWD/../../run_benchmarks/run_nas_benchmark
 file=$PWD/../res/nas_overhead.csv
+exit=$PWD/../res/overhead.csv
 
-touch $file
+touch $file $exit
 
     
 echo "NAME,TIME" >> $file
 
 for app in $nas_dir/log/* ; do 
-    echo $(basename $app) | tr '\n' ',' >> $file
-    grep -e "Time in seconds" $app | sed -e "s/Time in seconds =//g" | sed -e "s/ //g"  >> $file
+    echo $(basename $app) | cut -d '.' -f 1,2,3 | tr '\n' ',' >> $file
+    grep -e "Time in seconds" $app | sed -e "s/Time in seconds =//g" | sed -e "s/ //g" >> $file
+done
+
+
+echo "NAME,DIFF" >> "$exit"
+tail -n +2 "$file" | while read -r line1 && read -r line2 ; do 
+    name1=$(echo "$line1" | cut -d ',' -f1)
+    name2=$(echo "$line2" | cut -d ',' -f1)
+    time1=$(echo "$line1" | cut -d ',' -f2)
+    time2=$(echo "$line2" | cut -d ',' -f2)
+
+    diff=$(echo "$time1 - $time2" | bc -l)
+    diff=$(echo "$diff" | sed 's/^-//' )
+
+    echo "$name1,$diff" >> $exit
+
 done
