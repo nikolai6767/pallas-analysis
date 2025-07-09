f939042fccf2b7a86a175a8e2c09730e6914f132
MONTICELLI Nikolai <n.monticelli2016@gmail.com>
2025-07-09 09:03:57 +0200

    gitignore

---
+++ analysis/prog/lulesh_trace_size.sh
@@ -0,0 +1,14 @@
+#!/bin.sh
+
+lulesh_dir=$PWD/../../run_benchmarks/run_lulesh
+file=$PWD/../res/lulesh_trace_size.csv
+
+touch $file
+    
+echo "NOM TAILLE" >> $file
+for app in $lulesh_dir/traces/* ; do 
+    if [ -d "$app" ]; then
+        app_name=$(basename app)
+        du -sh $app |cut -d' ' -f1 >> $file
+    fi
+done
---
+++ analysis/prog/nas_trace_size.sh
@@ -0,0 +1,14 @@
+#!/bin.sh
+
+nas_dir=$PWD/../../run_benchmarks/run_nas_benchmark
+file=$PWD/../res/nas_trace_size.csv
+
+touch $file
+    
+echo "NOM TAILLE" >> $file
+for app in $nas_dir/traces/* ; do 
+    if [ -d "$app" ]; then
+        app_name=$(basename app)
+        du -sh $app |cut -d' ' -f1 >> $file
+    fi
+done
