a9dd01743512a018a634be83e8363347ef86bd03
MONTICELLI Nikolai <n.monticelli2016@gmail.com>
2025-08-11 09:13:23 +0200

    Retour à vector size = 1000

--- libraries/pallas/include/pallas/pallas_linked_vector.h
+++ libraries/pallas/include/pallas/pallas_linked_vector.h
@@ -20,7 +20,7 @@
 #include <utility>
 #include <vector>
 /** Default size for creating Vectors and SubVectors.*/
-#define DEFAULT_VECTOR_SIZE 1000000
+#define DEFAULT_VECTOR_SIZE 1000
 
 namespace pallas {
 /**
